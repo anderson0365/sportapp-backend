21ec4674cfe3263e379a6f5d82a47cf46a219f03
cperafan <cperafan@mercadolibre.com.co>
2022-11-05 17:48:43 -0500

    change beginning api url

--- training/src/main.ts
+++ training/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableVersioning({
     type: VersioningType.URI,
-    prefix: 'api/v',
+    prefix: '/sv-training/api/v',
     defaultVersion: '1',
   });
   await app.listen(3002);
