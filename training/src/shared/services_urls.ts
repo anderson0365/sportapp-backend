4734a2adc7980d2c29f311bee39e4cdb1bfbee83
cperafan <cperafan@mercadolibre.com.co>
2022-11-05 17:52:15 -0500

    change beginning api url

--- training/src/shared/services_urls.ts
+++ training/src/shared/services_urls.ts
@@ -1,2 +1,2 @@
 
-export const athleteUserManagmentServiceURL = process.env.NODE_ENV === 'prod' ? process.env.PROD_URL : 'http://localhost:3000/api/v1';
+export const athleteUserManagmentServiceURL = process.env.NODE_ENV === 'prod' ? process.env.PROD_URL : 'http://localhost:3000/sv-athlete/api/v1';
