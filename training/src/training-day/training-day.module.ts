a6d076405c03df5ef908c7e0178dee41894fdfdf
Joan Blanco <78229149+jblancov7764@users.noreply.github.com>
2022-11-07 21:00:57 -0500

    Se agrega metodos para generar y persistir eventos

--- training/src/training-day/training-day.module.ts
+++ training/src/training-day/training-day.module.ts
@@ -1,15 +1,18 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActivityService } from 'src/activity/activity.service';
+import { PlaceService } from 'src/place/place.service';
 import { AthleteService } from '../athlete/athlete.service';
-import { TrainingPlanEntity } from '../training-plan/training-plan.entity';
 import { TrainingDayController } from './training-day.controller';
 import { TrainingDayEntity } from './training-day.entity';
 import { TrainingDayService } from './training-day.service';
+import { ActivityEntity } from '../activity/activity.entity';
+import { PlaceEntity } from 'src/place/place.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TrainingPlanEntity, TrainingDayEntity]), HttpModule],
-  providers: [AthleteService, TrainingDayService],
+  imports: [TypeOrmModule.forFeature([TrainingDayEntity, ActivityEntity, PlaceEntity]), HttpModule],
+  providers: [AthleteService, TrainingDayService, ActivityService, PlaceService],
   controllers: [TrainingDayController]
 })
 export class TrainingDayModule {}
