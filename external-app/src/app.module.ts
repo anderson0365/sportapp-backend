55cfd21e3c5a69e0b1b09db8f7623ac89788e2ab
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2022-11-12 16:56:59 -0500

    External App added

---
+++ external-app/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ActivityModule } from './activity/activity.module';
+
+@Module({
+  imports: [ActivityModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
